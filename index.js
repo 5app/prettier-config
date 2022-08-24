c2d2e39a332165a1a79f9626305a8c8bc5bf8a09
Andrew Dodson <andrew@5app.com>
2022-08-24 13:59:14 +0100

    feat(init): initial config

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 /**
- * Entry point to initiate the project
+ * Shareable prettier config
  */
+const config = {
+	arrowParens: 'avoid',
+	bracketSpacing: false,
+	semi: true,
+	singleQuote: true,
+	tabWidth: 4,
+	trailingComma: 'es5',
+	useTabs: true,
+};
+
+export default config;
